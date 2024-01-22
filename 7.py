01455e573c337da23e438ed8aba0557d78201237
NazarSnishko <123537311+NazarSnishko@users.noreply.github.com>
2024-01-22 02:51:44 +0200

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,5 @@
+start = int(input("Введіть початок діапазону: "))
+end = int(input("Введіть кінець діапазону: "))
+for number in range(start, end+1, 1):
+    print(" ",number)
+    # TODO: write code...
